9e8d545d68b9f8f1cbc6e49b086c47129924df4d
Miron Dima <mirontsov2098@mail.ru>
2023-03-19 19:58:51 +0300

    add lesson22

---
+++ src/modules/scroll.js
@@ -0,0 +1,33 @@
+const scroll = () => {
+	const menuItems = document.querySelectorAll('ul > li > a')
+	const anchorBtnService = document.querySelectorAll('a[href = "#service-block"]')
+
+
+	menuItems.forEach(menuItems => {
+		menuItems.addEventListener('click',(e) => {
+			e.preventDefault()
+			const blockId = menuItems.getAttribute('href').substring(1)
+			document.getElementById(blockId).scrollIntoView({
+				behavior: 'smooth',
+				block: "center",
+				inline: 'center'
+			})
+		})
+	})
+
+
+	anchorBtnService.forEach(btn => {
+		btn.addEventListener('click', (e) => {
+			e.preventDefault()
+			const btnSecvice = btn.getAttribute('href').substring(1)
+			document.getElementById(btnSecvice).scrollIntoView({
+				behavior: 'smooth',
+				block: 'center'
+			})
+		})
+	})
+
+
+}
+
+export default scroll
---
+++ src/modules/tabs.js
@@ -0,0 +1,26 @@
+const tabs = () => {
+
+const tabContent = document.querySelectorAll('.service-tab')
+const tabs = document.querySelectorAll('.service-header-tab')
+const tabPanel = document.querySelector('.service-header')
+
+tabPanel.addEventListener('click', (e) => {
+	
+	if(e.target.closest('.service-header-tab')){
+		const newTabs = e.target.closest('.service-header-tab')			//чтобы наши табы переклюались вместе с спаном,мы сравниваем с нашей новой кнопкой
+		
+		tabs.forEach((tab, index) => {
+			if(tab === newTabs){
+				tab.classList.add('active')
+				tabContent[index].classList.remove('d-none')
+
+			}else{
+				tab.classList.remove('active')
+				tabContent[index].classList.add('d-none')
+			}
+		});
+	}
+})
+}
+
+export default tabs
