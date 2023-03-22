8b4a3689d79590f3452487a740bc0ebacc155c33
Miron Dima <mirontsov2098@mail.ru>
2023-03-22 14:56:57 +0300

    add lesson24hard

--- src/modules/culc.js
+++ src/modules/culc.js
@@ -1,3 +1,5 @@
+import animateNum from './animateNum'
+
 const culc = (price = 100) => {
 
 	const calcBlock = document.querySelector('.calc-block')
@@ -5,37 +7,41 @@ const culc = (price = 100) => {
 	const calcSquare = document.querySelector('.calc-square')
 	const calcCount = document.querySelector('.calc-count')
 	const calcDay = document.querySelector('.calc-day')
-	const total = document.getElementById('total')
-
+	let total = document.getElementById('total')
+	let totalPrice = 0
 
 	const countCulc = () => {
 		const culcTypeValue = +culcType.options[culcType.selectedIndex].value
 		const calcSquareValue = calcSquare.value
-		let totalValue = 0
+		
 		let culcCountValue = 1
 		let calcDayValue = 1
 
 		if (calcCount.value > 1) {
 			culcCountValue = culcCountValue + (calcCount.value / 10)
-			console.log(culcCountValue);
+			console.log(calcCount.value);
 		}
 
-		if(calcDay.value && calcDay.value < 5){
+		if (calcDay.value && calcDay.value < 5) {
 			calcDayValue = 2
-		}else if(calcDay.value && calcDay.value < 10){
+		} else if (calcDay.value && calcDay.value < 10) {
 			calcDayValue = 1.5
+			console.log(calcDay.value);
 		}
 
 
 		if (culcType.value && calcSquare.value) {
 			console.log(culcType.value);
 			console.log(calcSquare.value);
-			totalValue = price * culcTypeValue * calcSquareValue * culcCountValue * calcDayValue
-			console.log(totalValue)
+			totalPrice = price * culcTypeValue * calcSquareValue * culcCountValue * calcDayValue
+			animateNum(totalPrice,total)
 		} else {
-			totalValue = 0
+			totalPrice = 0
 		}
-		total.textContent = totalValue
+		
+		total.textContent = totalPrice
+		
+		
 	}
 
 	calcBlock.addEventListener('input', (e) => {
@@ -43,9 +49,11 @@ const culc = (price = 100) => {
 		if (e.target === culcType || e.target === calcSquare ||
 			e.target === calcCount || e.target === calcDay) {
 			countCulc()
+			
 		}
 
 	})
 
 }
+
 export default culc
